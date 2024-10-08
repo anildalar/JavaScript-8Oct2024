c83412d5566468a7e8624d08cde710e08d97a281
anildalar <dollorinfotech@gmail.com>
2024-10-08 18:41:34 +0530

    new

---
+++ test.js
@@ -0,0 +1,15 @@
+var name="OKLABS";//String
+let student1 = 'Vishal';//String
+const student2 = `Abhishek`;//String
+student3 = "Gaurav"; //String
+
+var age = 30; //Number
+//Every line is called statement
+// Every statement is terminated by semicolon ;
+// Single line command i can write anything
+
+console.log(name);
+console.log(student1);
+console.log(student2);
+console.log(student3);
+console.log(age);
